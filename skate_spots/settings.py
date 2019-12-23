77ee4aa5f46d417b71be030a28408a910c744602
Nick <nicholasevans17@gmail.com>
2019-12-23 12:55:49 -0800

    s3 bucket for media storage

--- skate_spots/settings.py
+++ skate_spots/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -124,5 +125,30 @@ STATIC_URL = '/static/'
 GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyADsKSJUx6DD63P26FsBj9BWC1vpByW1wo'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
+
+AWS_ACCESS_KEY_ID = 'AKIAWYWMNNP3JOLCM36P'
+
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = 'django-skate-spots'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
